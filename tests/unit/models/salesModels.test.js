1907c97a24fc4ea17144d803077afd744bb577de
João <joaolelles7@gmail.com>
2023-02-20 15:26:01 -0300

    Realizando dois testes em sales model

---
+++ tests/unit/models/salesModels.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { salesModel } = require('../../../src/models');
+const conn = require('../../../src/models/connection');
+const { sales, saleById } = require('./mocks/sales.model.mock');
+
+describe('Testando a camada model de sales', function () {
+  it('recuperando todas sales', async function () {
+    // Arrange
+    sinon.stub(conn, 'execute').resolves([sales]);
+    // Act
+    const result = await salesModel.selectAll();
+    // Assert
+    expect(result).to.be.deep.equal(sales);
+  });
+  it('recuperando uma sale pelo id', async function () {
+    sinon.stub(conn, 'execute').resolves([saleById]);
+    const sale = await salesModel.selectById(1);
+
+    expect(sale).to.be.deep.equal(saleById);
+  });
+
+    afterEach(function () {
+    sinon.restore();
+  });
+});
